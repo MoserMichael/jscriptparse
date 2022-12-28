24fd534c4c761da4e450ef026afc41a9f25bc9f1
mmoser-blaze <mmoser@perforce.com>
2022-12-28 09:09:31 +0200

    docs

--- build-docs.sh
+++ build-docs.sh
@@ -4,14 +4,18 @@ git checkout main
 jsdoc prs.js README.md
 
 tmp=$(mktemp -d)
-mv out ${tmp}/
+mv out "${tmp}/"
 
+BRANCH=$(git branch --show-current)
 git checkout gh-pages 
 
-cp -rf ${tmp}/out .
+cp -rf "${tmp}/out" .
+rm -rf "${tmp}"
 
 git add out/* || true
 
 today=$(date)
 
 git commit -am "documentation changes ${today}"
+
+git checkout "${BRANCH}"

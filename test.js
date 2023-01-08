ff72a8b71f9c2a44d0200d91dd0a323db38e437c
mmoser-blaze <mmoser@perforce.com>
2023-01-08 21:22:37 +0200

    tests

--- test.js
+++ test.js
@@ -116,11 +116,11 @@ product: 6 sum product of squares: 13
     a = [1, 2, 3, 4]
     b = map(a, def(x) { return x * x })
     
-    print("b: {b[0]} {b[1]} {b[2]} {b[3]}")
+    println("b: {b[0]} {b[1]} {b[2]} {b[3]}")
     
     c = reduce(b, def(x, y) { return x + y }, 0)
-    println(c)        
-`, "b: 1 4 9 1630\n"),
+    println("sum of squares: {c}")
+`, "b: 1 4 9 16\nsum of squares: 30\n"),
 
     new TestCase(`    
     def rec(num) {

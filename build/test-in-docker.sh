50c4360c10b53bee441eccb1e2e93512bb3555b3
mmoser-blaze <mmoser@perforce.com>
2023-02-14 02:54:40 +0200

    fix docker test

--- build/test-in-docker.sh
+++ build/test-in-docker.sh
@@ -4,4 +4,6 @@ set -ex
 
 docker rmi pyx-test:latest || true
 docker build . -f build/Dockerfile -t pyx-test:latest
-docker run --rm -v $PWD:/mnt pyx-test:latest  bash -c 'cd  /mnt; make test-installed '
+docker run --rm -v $PWD:/mnt pyx-test:latest  bash -c 'npm install pyxlang -g; cd  /mnt; make test-installed '
+
+
